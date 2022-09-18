472e8a01dabcb6187124a5d13e3d7bd93b43e30b
Huseyn <hsynbsv15@gmail.com>
2022-09-18 20:11:27 +0400

    Add project files.

---
+++ Gallery/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gallery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public ObservableCollection<UserControl1> Photos { get; set; }
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //    this.DataContext = this;
+        //    Photos = new ObservableCollection<UserControl1>();
+        //}
+        //private void AddFile_MenuItem_Click(object sender, RoutedEventArgs e)
+        //{
+        //    OpenFileDialog op = new OpenFileDialog();
+        //    op.Title = "Select a picture";
+        //    op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+        //      "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+        //      "Portable Network Graphic (*.png)|*.png";
+        //    if (op.ShowDialog() == true)
+        //    {
+        //        UserControl userControl = new UserControl();
+
+        //        //.Source = new BitmapImage(new Uri(op.FileName));
+        //    }
+        //}
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    OpenFileDialog op = new OpenFileDialog();
+        //    op.Title = "Select a picture";
+        //    op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+        //      "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+        //      "Portable Network Graphic (*.png)|*.png";
+        //    if (op.ShowDialog() == true)
+        //    {
+        //        if(op.CheckFileExists == true)
+        //        {
+        //            UserControl1 userControl1 = new UserControl1();
+        //            userControl1.MouseEnter += UserControl1_MouseEnter;
+        //            userControl1.MouseLeave += UserControl1_MouseLeave;
+        //            userControl1.MouseUp += UserControl1_MouseUp;
+        //            userControl1.Width = 200;
+        //            userControl1.Height = 200;
+        //            userControl1.img.Source = new BitmapImage(new Uri(op.FileName));
+        //            Photos.Add(userControl1);
+
+        //        }
+
+        //    }
+        //}
+
+        //private void UserControl1_MouseUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    var sender2 = sender as UserControl1;
+        //    UserControl2 userControl2 = new UserControl2();
+        //    userControl2.Img = new BitmapImage(new Uri(sender2.img.Source.ToString()));
+        //    userControl2.Photos = Photos;
+        //    MainGrid.Children.Add(userControl2);
+        //}
+
+        //private void UserControl1_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    var sender2 = sender as UserControl1;
+        //    sender2.BorderBrush = null;
+
+        //}
+
+        //private void UserControl1_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    var sender2 = sender as UserControl1;
+        //    sender2.BorderThickness = new Thickness(1.5);
+        //    sender2.BorderBrush = Brushes.Blue;
+        //}
+
+        public ObservableCollection<Image> Photos { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Photos = new ObservableCollection<Image>();
+        }
+        private void AddFile_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                UserControl userControl = new UserControl();
+
+                //.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                if (op.CheckFileExists == true)
+                {
+
+                    Image newPhoto = new Image();
+                    newPhoto.Source = new BitmapImage(new Uri(op.FileName));
+                    newPhoto.Height = 200;
+                    newPhoto.Width = 200;
+                    Photos.Add(newPhoto);
+
+                }
+
+            }
+        }
+        private void MainListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var photo = MainListBox.SelectedItem as Image;
+
+            UserControl2 userControl2 = new UserControl2();
+            //userControl2.Img = new BitmapImage(new Uri(photo.Source.ToString()));
+            userControl2.Img1 = photo;
+            userControl2.Photos = Photos;
+            MainGrid.Children.Add(userControl2);
+        }
+    }
+}
---
+++ Gallery/UserControl2.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gallery
+{
+    /// <summary>
+    /// Interaction logic for UserControl2.xaml
+    /// </summary>
+    public partial class UserControl2 : UserControl
+    {
+        //public ObservableCollection<UserControl1> Photos { get; set; }
+        //public BitmapImage Img
+        //{
+        //    get { return (BitmapImage)GetValue(ImgProperty); }
+        //    set { SetValue(ImgProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for Img.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty ImgProperty =
+        //    DependencyProperty.Register("Img", typeof(BitmapImage), typeof(UserControl2));
+
+
+        //public UserControl2()
+        //{
+        //    InitializeComponent();
+
+        //    this.DataContext = this;
+        //}
+
+        //private void BackButton_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //    this.Visibility = Visibility.Collapsed;
+        //}
+
+        //private void PrevBtn_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //    //MessageBox.Show($"{this.Parent.GetType()}");
+        //}
+
+        public ObservableCollection<Image> Photos { get; set; }
+
+
+        public Image Img1
+        {
+            get { return (Image)GetValue(Img1Property); }
+            set { SetValue(Img1Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Img1.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty Img1Property =
+            DependencyProperty.Register("Img1", typeof(Image), typeof(UserControl2));
+
+
+        //public BitmapImage Img
+        //{
+        //    get { return (BitmapImage)GetValue(ImgProperty); }
+        //    set { SetValue(ImgProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for Img.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty ImgProperty =
+        //    DependencyProperty.Register("Img", typeof(BitmapImage), typeof(UserControl2));
+
+
+        public UserControl2()
+        {
+            InitializeComponent();
+           
+            this.DataContext = this;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        private void PrevBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            int index = Photos.IndexOf(Img1);
+            if(index > 0)
+            {
+                Img1 = Photos[index - 1];
+            }
+            //Img1 = null;
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int index = Photos.IndexOf(Img1);
+            
+            if (index < Photos.Count-1)
+            {
+                Img1 = Photos[index + 1];
+            }
+        }
+    }
+}
